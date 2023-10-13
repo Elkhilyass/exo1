c0b04c343a49798eae786ea7faba3461adcdea4f
El Khazane <ilyasekhazane2003@gmail.com>
2023-10-13 09:27:44 +0100

    exercice 1

---
+++ exo2/test_exo2.py
@@ -0,0 +1,35 @@
+import unittest
+
+def fonction(string,ending):
+    return string.endswith(ending)  
+
+class Exo2Test(unittest.TestCase):
+
+    def test_true_cases(self):
+        fixed_tests_True = (
+            ("samurai", "ai"),
+            ("ninja", "ja"),
+            ("sensei", "i"),
+            ("abc", "abc"),
+            ("abcabc", "bc"),
+            ("fails", "ails"),
+        )
+        for string, ending in fixed_tests_True:
+            self.assertEqual(fonction(string, ending),True)
+    
+    
+    def test_false_cases(self):
+        fixed_tests_False = (
+            ("sumo", "omo"),
+            ("samurai", "ra"),
+            ("abc", "abcd"),
+            ("ails", "fails"),
+            ("this", "fails"),
+            ("spam", "eggs"),
+        )
+        for string, ending in fixed_tests_False:
+            self.assertEqual(fonction(string, ending),False)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- exo3/test_exo3.py
+++ exo3/test_exo3.py
@@ -1,16 +1,42 @@
 import unittest
 
-from exo3 import processLines
-
+def processLines(lines) -> str:
+        N=int(lines[0])
+        T=int(lines[1])
+        backlog = T
+        for i in range(2,N+2):
+                V,U = map(int, lines[i].split())
+                backlog = backlog- V + U
+                
+                if backlog < 0:
+                        return "KO"
+                     
+        if backlog == 0:
+                return "OK"
+        else:
+                return "KO"
 
 class TestExo3(unittest.TestCase):
     def test_input_1(self):
-        with open("sample/input1.txt") as input1:
+        with open("exo3/sample/input1.txt") as input1:
             lines = input1.readlines()
 
-        with open("sample/output1.txt") as output1:
+        with open("exo3/sample/output1.txt") as output1:
             expected = output1.read()
 
         self.assertEqual(expected, processLines(lines))
+    
+    def test_input_2(self):
+        with open("exo3/sample/input2.txt") as input2:
+            lines = input2.readlines()
+
+        with open("exo3/sample/output2.txt") as output2:
+            expected = output2.read()
+
+        self.assertEqual(expected, processLines(lines))
+
+
+if __name__ == '__main__':
+    unittest.main()
 
     # Ecrire une autre méthode pour vérifier le second use case
